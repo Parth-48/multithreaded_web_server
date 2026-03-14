3bd45f90f3d5bc3dc54bc4a55f842ef2ba079c5d
Parth-48 <parthukarde48@gmail.com>
2026-03-14 13:48:25 +0530

    threadpool architechture

--- src/threadpool.c
+++ src/threadpool.c
@@ -8,9 +8,11 @@ SOCKET task_queue[QUEUE_SIZE];
 
 int front=0;
 int rear=0;
+int count=0;
 
 HANDLE mutex;
 HANDLE task_available;
+HANDLE queue_space;
 
 DWORD WINAPI worker_thread(void *client_socket_ptr){
     while(1){
@@ -20,17 +22,23 @@ DWORD WINAPI worker_thread(void *client_socket_ptr){
 
         SOCKET client_fd = task_queue[front];
         front = (front+1) % QUEUE_SIZE;
+        count--;
 
         ReleaseMutex(mutex);
+
+        ReleaseSemaphore(queue_space,1,NULL);
 
         handle_request(client_fd);
         closesocket(client_fd);
     }
+
+    return 0;
 }
 
 void threadpool_init(){
     mutex = CreateMutex(NULL,FALSE,NULL);
     task_available = CreateSemaphore(NULL,0,QUEUE_SIZE,NULL);
+    queue_space = CreateSemaphore(NULL,QUEUE_SIZE,QUEUE_SIZE,NULL);
 
     for(int i=0; i<THREAD_COUNT; i++){
         CreateThread(NULL,0,worker_thread,NULL,0,NULL);
@@ -38,10 +46,13 @@ void threadpool_init(){
 }
 
 void add_task(SOCKET client_fd){
+    WaitForSingleObject(queue_space,INFINITE);
+
     WaitForSingleObject(mutex, INFINITE);
 
     task_queue[rear] = client_fd;
     rear = (rear+1) % QUEUE_SIZE;
+    count++;
 
     ReleaseMutex(mutex);
 
